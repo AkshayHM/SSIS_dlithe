e3400717342fb253ad9b4811535b82547531f431
AkshayHM <akshayhiremath2018@gmail.com>
2022-10-04 14:01:31 +0530

    Update Referencal Integrity.sql

--- Referencal Integrity.sql
+++ Referencal Integrity.sql
@@ -181,11 +181,6 @@ select * from router1;
 SELECT 
 DENSE_RANK() OVER (partition by Department_id order by salary DESC) AS D_RANK,DEPARTMENT_ID,Employee_ID,salary
 FROM s_hremployeeS;
-
-
-
-
-
 -------------------------------------------------------------------
 create table 
 
