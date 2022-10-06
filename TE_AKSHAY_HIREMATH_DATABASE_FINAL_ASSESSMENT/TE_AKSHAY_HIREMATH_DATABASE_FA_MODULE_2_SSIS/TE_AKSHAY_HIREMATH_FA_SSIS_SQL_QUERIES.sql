8311f7b77c87d4b5d42383c290089fb3b0b5e722
AkshayHM <akshayhiremath2018@gmail.com>
2022-10-06 22:23:48 +0530

    Create TE_AKSHAY_HIREMATH_FA_SSIS_SQL_QUERIES.sql

---
+++ TE_AKSHAY_HIREMATH_DATABASE_FINAL_ASSESSMENT/TE_AKSHAY_HIREMATH_DATABASE_FA_MODULE_2_SSIS/TE_AKSHAY_HIREMATH_FA_SSIS_SQL_QUERIES.sql
@@ -0,0 +1,18 @@
+create table TE_FA_Cinema_Ticket_Target
+(
+Film_Type varchar(250),
+film_code varchar(250),
+cinema_code varchar(250),
+total_sales int,
+tickets_sold int,
+tickets_out	int,
+show_time time,
+occu_perc varchar(250),
+ticket_price float,
+ticket_use	int,
+capacity varchar(250),
+show_date date	,
+show_month int,
+show_quarter int,
+show_day varchar(250)
+);
